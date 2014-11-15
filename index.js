3bc64cb0ea70241000a4559e56dd0fcd5b6ff187
magicode <magicode7@gmail.com>
2014-11-16 00:56:18 +0200

    update

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ var nlp =  new addon.NatLookup();
 
 
 function getOrginalDst(socket,cb){
+	if(!socket || !socket._handle ||  !socket._handle.fd) return cb("no socket");
 	nlp.natLookup(socket._handle.fd, socket.localPort , socket.localAddress ,cb);
 }
 
